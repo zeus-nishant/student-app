0b417abfd3ec9c4af5777663094b3bd7d25e6ff9
nishant-zeus <nishantpal1409@gmail.com>
2025-03-13 23:22:36 +0530

    api changes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3002";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api`,
   validateStatus: (status) => status < 500, // Resolve if status is below 500
 });
 
